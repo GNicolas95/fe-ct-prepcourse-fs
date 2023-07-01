71dfc32618196badbc5a4bc41ea873a4b4e12640
GNicolas95 <geroma16@gmail.com>
2023-07-01 12:08:29 -0300

    Resolucion

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,14 +6,33 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arrayDeArray = [];
+   var claves = Object.keys(objeto);
+   for (var i = 0; i < claves.length; i++) {
+      var clave = claves[i];
+      var valor = objeto[clave];
+      arrayDeArray.push([clave, valor]);
+   }
+      return (arrayDeArray);
 }
 
 function numberOfCharacters(string) {
    // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
    // letras del string, y su valor es la cantidad de veces que se repite en el string.
-   // Las letras deben estar en orden alfabético.
+   // Las letras deben estar en orden alfabético.//
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var objeto = {};
+   var array = string.split("").sort().join("")
+   for (var i = 0; i < array.length; i ++) {
+      var letra = array[i];
+      if (letra in objeto) {
+         objeto[letra]++;
+      } else {
+         objeto[letra] = 1;
+      }
+   }
+      return (objeto);
 }
 
 function capToFront(string) {
@@ -22,6 +41,16 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayuscula = "";
+   var minuscula = "";
+   for (var i = 0; i < string.length; i++) {
+      if (string[i].toUpperCase() === string[i]) {
+         mayuscula = mayuscula + string[i];
+      } else {
+         minuscula = minuscula + string[i];
+      }
+   }
+      return (mayuscula + minuscula);
 }
 
 function asAmirror(frase) {
@@ -29,18 +58,43 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var resultado = [];
+   var palabras = frase.split(" ");
+   for (var i = 0; i < palabras.length; i++) {
+      resultado = resultado + palabras[i].split("").reverse().join("");
+      if (i != palabras.length - 1) {
+         resultado = resultado + " ";
+      }  
+   }
+      return (resultado);
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var str = numero.toString();
+   var resultado = "Es capicua";
+   for (var i = 0; i < str.length; i++) {
+      if (str[i] != str[str.length - 1 - i]) {
+         resultado = "No es capicua";
+      }
+   }
+      return (resultado);
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var resultado = "";
+   for ( var i = 0; i < string.length; i++) {
+      if (string[i] != "a" && string[i] != "b" && string[i] != "c") {
+         resultado = resultado + string[i];
+      }
+   }
+      return (resultado);
+
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +103,8 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   arrayOfStrings = arrayOfStrings.sort((a, b) => a.length - b.length);
+      return (arrayOfStrings);
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +114,13 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var arreglo = [];
+   for (var i = 0; i < array2.length; i++) {
+      if (array1.includes(array2[i])) {
+         arreglo.push(array2[i])
+      }
+   }
+      return (arreglo);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
