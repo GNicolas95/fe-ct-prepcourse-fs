e5c5dabd9f32c54655294da72dc17e69d7560cf4
GNicolas95 <geroma16@gmail.com>
2023-07-19 12:36:29 -0300

    Resolucion 3

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -152,13 +152,13 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   if (num1 > num2 && num3) {
+   if (num1 > num2 && num1 > num3 && num1 > 0) {
       return ("Numero 1 es mayor y positivo");
-   } else if (num1 == -1 || num2 == 50 || num3 == 2) {
+   } else if (num1 < 0 || num2 < 0 || num3 < 0) {
       return ("Hay negativos");
-   } else if (num3 > num2 && num1) {
+   } else if (num3 > num2 && num3 > num1) {
       return (num3 + 1);
-   } else if (num1, num2, num3 == 0) {
+   } else if (num1 == 0 && num2 == 0 && num3 == 0) {
       return ("Error");
    } else {
       return (false);
